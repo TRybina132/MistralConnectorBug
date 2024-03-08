5e1b47c76838f8cf8cab91ee763b0228f68e84f0
TRybina132 <rybina@managed-code.com>
2024-03-08 16:35:01 +0200

    add example code

--- MistralBug/Program.cs
+++ MistralBug/Program.cs
@@ -1,3 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
 
-Console.WriteLine("Hello, World!");
+// TODO: Add here your credentials
+var kernelBuilder = Kernel.CreateBuilder()
+    .AddMistralChatCompletion(
+        apiKey: "",
+        modelId: "mistral-small",
+        serviceId: "mistral");
+
+kernelBuilder.Services.AddLogging(c =>
+{
+    c.AddDebug().SetMinimumLevel(LogLevel.Trace);
+    c.AddSimpleConsole(options =>
+    {
+        options.IncludeScopes = true;
+        options.SingleLine = true;
+        options.TimestampFormat = "hh:mm:ss ";
+    });
+});
+
+var kernel = kernelBuilder.Build();
+
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+ChatHistory history = new ChatHistory();
+
+history.AddUserMessage("How much will be 10 + 2?");
+
+var result = await chatCompletionService.GetChatMessageContentAsync(
+    history,
+    kernel: kernel);
+
+string fullMessage = result.Content;
+
+Console.WriteLine(fullMessage);
+
+var streamingResult = chatCompletionService.GetStreamingChatMessageContentsAsync(
+    history,
+    kernel: kernel);
+
+var resultStringBuilder = new StringBuilder();
+await foreach (var content in streamingResult)
+{
+    resultStringBuilder.Append(content);
+}
+
+Console.WriteLine(resultStringBuilder.ToString());
